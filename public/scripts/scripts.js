58a3a0fa2ec6bf7994482b72a558da402e33f8b1
unknown <jonlynch@US6498292-X7001.clients.us.kworld.kpmg.com>
2014-06-05 18:26:05 -0500

    updated scripts for google analytics

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -82,18 +82,16 @@ setPortfolioAnimations = function(){
   });
 
   win.scroll(function(event) {
-    
     allMods.each(function(i, el) {
       var el = $(el);
       if (el.visible(true)) {
         el.addClass("come-in"); 
       } 
     });
-    
   });
 }
 
-function resizeNavAnimation() {
+resizeNavAnimation = function() {
   var activeNavItem = $('header a.active');
   if(activeNavItem.length){
     var itemWidth = $(activeNavItem)[0].clientWidth;
@@ -102,3 +100,12 @@ function resizeNavAnimation() {
     navAnim.animate(navAnim.canvas, navAnim.context, left, itemWidth);  
   }
 };
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-51701738-1', 'thejonlynch.com');
+ga('send', 'pageview');
