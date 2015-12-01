8133cb8ba777e751fe72321299cb7a48c3d37bd3
Jon Lynch <jon.lynch@dealertrack.com>
2015-12-01 14:43:39 -0600

    added and configured blog

---
+++ hexo/source/_posts/First-Post.md
@@ -0,0 +1,17 @@
+title: First Post
+date: 2015-12-01 14:39:15
+tags:
+---
+Welcome to my blog! I don't have a ton to say here other than this is my first post. I've done blogging before here and there with wordpress, blogger, etc. but I don't feel any of those old posts are relevant to my career anymore. So... goodbye old and hello new!
+
+I'm using [Hexo](http://hexo.io/). Hexo is a static file generator, not a traditional blogging platform. I can create a new blog post via command line like-a-so:
+
+``` bash
+$ hexo new "My New Post"
+```
+
+Then I can deploy this to Heroku or GitHub and voila!
+
+I'll be blogging about life, work, all things javascript, etc. so check back!
+
+-Jon
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,29 +1,39 @@
 (function(window, $){
-  var adjustment = 85;
+  window.adjustment = 85;
 
 	// Init Canvas Animation
 	window.navAnim = new CanvasNavAnimation("navCanvas");
 
   // First Item Animation
   $(window).load(function(){
-    var firstItem = $('#firstNavItem');
+    var firstItem = '';
+
+    if($('a[href=' + location.hash +']').length > 0){
+      firstItem = $('a[href=' + location.hash +']');
+    } else {
+        firstItem = $('nav').find('.active').length > 0 ? $('nav').find('.active') : $('#firstNavItem');
+    }
+
     var itemWidth = $(firstItem)[0].clientWidth;
     var left = $(firstItem).offset().left - $('header .row')[0].offsetLeft;
-    
+
     navAnim.stopAnim = false;
-    
+
     setTimeout(function(){
         navAnim.animate(navAnim.canvas, navAnim.context, left, itemWidth);
     }, 800);
 
     $(firstItem).addClass('active');
+    scrollToPosition($(firstItem));
 
     // Set up animations for portfolio and logo
     setAnimations();
 
     //setScrollNavPosition();
     // Featured slider
-    $('#featured').orbit({ fluid: '16x6', captions: true, timer: true }); 
+    if($('#featured').length > 0){
+        $('#featured').orbit({ fluid: '16x6', captions: true, timer: true });
+    }
   });
 
 	// Nav animation
@@ -34,23 +44,7 @@
     navAnim.stopAnim = false;
     navAnim.animate(navAnim.canvas, navAnim.context, left, itemWidth);
     $(this).addClass('active');
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
-      var par=$(this).parent();
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          var topPosition = target.offset().top - adjustment;
-          if($(this).attr('href').indexOf('home') > -1){
-            topPosition = 0;
-          }
-          $('html,body').animate({
-             scrollTop: topPosition
-          }, 600,function(){}
-        );
-        return false;
-      }
-    }
-    
+    scrollToPosition($(this));
   });
 
   // If resize, re-size, position and animate the bar
@@ -61,18 +55,18 @@
   });
 
   // Tweet :)
-  $("#latestTweet .tweet").tweet({
-    join_text: "auto",
-    username: "jonlynch",
-    avatar_size: 48,
-    count: 1,
-    auto_join_text_default: "",
-    auto_join_text_ed: "",
-    auto_join_text_ing: "",
-    auto_join_text_reply: "",
-    auto_join_text_url: "",
-    loading_text: "loading tweets..."
-  });
+  // $("#latestTweet .tweet").tweet({
+  //   join_text: "auto",
+  //   username: "jonlynch",
+  //   avatar_size: 48,
+  //   count: 1,
+  //   auto_join_text_default: "",
+  //   auto_join_text_ed: "",
+  //   auto_join_text_ing: "",
+  //   auto_join_text_reply: "",
+  //   auto_join_text_url: "",
+  //   loading_text: "loading tweets..."
+  // });
 
   // Follow
   $('.follow').click(function(){
@@ -80,6 +74,27 @@
   });
 
 })(window, jQuery);
+
+scrollToPosition = function(origin){
+  // only do if not in /blog
+  if(origin[0].href.indexOf('/blog') === -1){
+    var par = origin.parent();
+    var targetHash = origin[0].toString().substring(origin[0].href.indexOf('#'));
+    var target = $(targetHash);
+    target = target.length ? target : $('[name=' + targetHash.slice(1) +']');
+    if (target.length) {
+      var topPosition = target.offset().top - adjustment;
+      if(origin.attr('href').indexOf('home') > -1){
+        topPosition = 0;
+      }
+      $('html,body').animate({
+         scrollTop: topPosition
+        }, 600,function(){}
+      );
+      return false;
+    }
+  }
+}
 
 setAnimations = function(){
   var win = $(window),
@@ -89,8 +104,8 @@ setAnimations = function(){
   allMods.each(function(i, el) {
     var el = $(el);
     if (el.visible(true)) {
-      el.addClass("is-visible"); 
-    } 
+      el.addClass("is-visible");
+    }
   });
 
   win.scroll(function(event) {
@@ -102,8 +117,8 @@ setAnimations = function(){
     allMods.each(function(i, el) {
       var el = $(el);
       if (el.visible(true)) {
-        el.addClass("come-in"); 
-      } 
+        el.addClass("come-in");
+      }
     });
   });
 }
@@ -127,7 +142,7 @@ setScrollNavPosition = function() {
         navAnim.stopAnim = false;
         navAnim.animate(navAnim.canvas, navAnim.context, left, itemWidth);
         activeNav.addClass('active');
-      } 
+      }
     });
   });
 }
@@ -138,7 +153,7 @@ resizeNavAnimation = function() {
     var itemWidth = $(activeNavItem)[0].clientWidth;
     var left = $(activeNavItem).offset().left - $('header .row')[0].offsetLeft;
     navAnim.stopAnim = false;
-    navAnim.animate(navAnim.canvas, navAnim.context, left, itemWidth);  
+    navAnim.animate(navAnim.canvas, navAnim.context, left, itemWidth);
   }
 };
 
