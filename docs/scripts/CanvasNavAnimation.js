1d6557a37a5308441e4fceffa37df5d17cbfef73
Jon Lynch <jonnylynchy@gmail.com>
2019-12-10 21:21:23 -0600

    added /docs folder for github pages

---
+++ docs/scripts/CanvasNavAnimation.js
@@ -0,0 +1,128 @@
+// A fun little experiement with canvas to implement an animated bar with active nav item - JL
+
+window.requestAnimFrame = (function(callback) {
+  return window.requestAnimationFrame || 
+  window.webkitRequestAnimationFrame || 
+  window.mozRequestAnimationFrame || 
+  window.oRequestAnimationFrame || 
+  window.msRequestAnimationFrame ||
+  function(callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+})();
+
+// Nav Canvas animations
+var CanvasNavAnimation = function(canvasElement){
+  this.canvas = document.getElementById(canvasElement);
+  this.context = this.canvas.getContext('2d');
+  this.x = 0;
+  this.y = 0;
+  this.WIDTH = 45;
+  this.HEIGHT = 5;
+  this.toX = 0;
+  this.toY = 0;
+  this.toWidth = 0;
+  this.toHeight = 5;
+  this.speed = 5;
+  this.barHeight = 3;
+  this.barWidth = 50;
+  this.rectX = 0;
+  this.rectY = 0;
+  this.linearSpeed = 15;
+  this.newX = 0;
+  this.newWidth = 0;
+  this.stopAnim = false;
+}
+
+CanvasNavAnimation.prototype.rect = function(x,y,w,h) {
+  this.ctx.fillStyle = "#ee7f38"
+  this.ctx.fillRect(x,y,w,h);
+  this.x = x;
+  this.y = y;
+  this.WIDTH = w;
+  this.HEIGHT = h;
+}
+
+CanvasNavAnimation.prototype.clear = function() {
+  this.ctx.clearRect(this.x, this.y, this.WIDTH, this.HEIGHT);
+}
+
+CanvasNavAnimation.prototype.startAnimation = function(x,y,w,h){
+  this.toX = x;
+  this.toY = y;
+  this.toWidth = w;
+  this.toHeight = h;
+  setTimeout(function(self){return self.draw();}(this), 100);
+}
+
+
+CanvasNavAnimation.prototype.animate = function(canvas, context, left, width) {
+  this.newWidth = width;
+
+  if(this.rectX < left && !this.stopAnim) {
+    this.newX = this.rectX + this.linearSpeed;
+    this.rectX = this.newX;
+    // clear
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // draw new rectangle
+    this.context.beginPath();
+    this.context.rect(this.rectX, this.rectY, width, this.barHeight);
+    this.context.fillStyle = "#f06607"
+    this.context.fill();
+
+    // request new frame
+    requestAnimFrame(function() {
+      window.navAnim.animate(window.navAnim.canvas, window.navAnim.context, left, width);
+    });
+
+    if(this.rectX >= left){
+      this.stopAnim = true;
+      // clear
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.rectX = left;
+      // draw new rectangle
+      this.context.beginPath();
+      this.context.rect(this.rectX, this.rectY, width, this.barHeight);
+      this.context.fillStyle = "#f06607"
+      this.context.fill();
+    }
+  } else if(this.rectX > left && !this.stopAnim){
+    this.newX = this.rectX - this.linearSpeed;
+    this.rectX = this.newX;
+    // clear
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // draw new rectangle
+    this.context.beginPath();
+    this.context.rect(this.rectX, this.rectY, width, this.barHeight);
+    this.context.fillStyle = "#ee7f38"
+    this.context.fill();
+
+    // request new frame
+    requestAnimFrame(function() {
+      window.navAnim.animate(window.navAnim.canvas, window.navAnim.context, left, width);
+    });
+
+    if(this.rectX <= left){
+      this.stopAnim = true;
+      // clear
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.rectX = left;
+      // draw new rectangle
+      this.context.beginPath();
+      this.context.rect(this.rectX, this.rectY, width, this.barHeight);
+      this.context.fillStyle = "#ee7f38"
+      this.context.fill();
+    }
+  }
+}
+
+CanvasNavAnimation.prototype.draw = function(){
+  this.clear();
+  if(this.x !== this.toX){
+    var left = this.toX > this.x ? this.x = this.x+this.speed : this.x = this.x-this.speed;
+    var width = this.toWidth > this.WIDTH ? this.WIDTH = this.WIDTH + this.speed : this.WIDTH = this.WIDTH - this.speed;
+    this.rect(this.toX,this.toY,this.toWidth,this.toHeight); 
+  }
+}
---
+++ docs/scripts/scripts.js
@@ -0,0 +1,183 @@
+(function(window, $){
+
+  window.adjustment = 85;
+
+	// Init Canvas Animation
+	window.navAnim = new CanvasNavAnimation("navCanvas");
+
+  // First Item Animation
+  $(window).load(function(){
+    var firstItem = '';
+
+    if($('a[href=' + location.hash +']').length > 0){
+      firstItem = $('a[href=' + location.hash +']');
+    } else {
+        firstItem = $('nav').find('.active').length > 0 ? $('nav').find('.active') : $('#firstNavItem');
+    }
+
+    var itemWidth = $(firstItem)[0].clientWidth;
+    var left = $(firstItem).offset().left - $('header .row')[0].offsetLeft;
+
+    navAnim.stopAnim = false;
+
+    setTimeout(function(){
+        navAnim.animate(navAnim.canvas, navAnim.context, left, itemWidth);
+    }, 800);
+
+    $(firstItem).addClass('active');
+    scrollToPosition($(firstItem));
+
+    // Set up animations for portfolio and logo
+    setAnimations();
+
+    //setScrollNavPosition();
+    // Featured slider
+    if($('#featured').length > 0){
+        $('#featured').orbit({ fluid: '16x6', captions: true, timer: true });
+    }
+  });
+
+	// Nav animation
+  var $navItems = $('header a, #mobile-nav li a');
+  $navItems.click(function(){
+    $navItems.removeClass('active');
+  	var itemWidth = $(this)[0].clientWidth;
+    var left = $(this).offset().left - $('header .row')[0].offsetLeft;
+    navAnim.stopAnim = false;
+    navAnim.animate(navAnim.canvas, navAnim.context, left, itemWidth);
+    $(this).addClass('active');
+    scrollToPosition($(this));
+  });
+
+  // Mobile Menu
+  var menuClicked = false;
+  var $menuTarget = $('#mobile-nav-toggle, #mobile-nav li a');
+
+  $menuTarget.on('click', function(){
+    if(menuClicked){
+      $('body').removeClass('mobile-nav-on');
+      menuClicked = false;
+    } else {
+      $('body').addClass('mobile-nav-on');
+      menuClicked = true;
+    }
+  });
+
+  // If resize, re-size, position and animate the bar
+  var resizeTimer;
+  $(window).bind('resize', function () {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(resizeNavAnimation, 100);
+  });
+
+  // Tweet :)
+  // $("#latestTweet .tweet").tweet({
+  //   join_text: "auto",
+  //   username: "jonlynch",
+  //   avatar_size: 48,
+  //   count: 1,
+  //   auto_join_text_default: "",
+  //   auto_join_text_ed: "",
+  //   auto_join_text_ing: "",
+  //   auto_join_text_reply: "",
+  //   auto_join_text_url: "",
+  //   loading_text: "loading tweets..."
+  // });
+
+  // Follow
+  $('.follow').click(function(){
+    window.open('http://twitter.com/jonlynch');
+  });
+
+})(window, jQuery);
+
+scrollToPosition = function(origin){
+  // only do if not in /blog
+  if(origin[0].href.indexOf('/blog') === -1){
+    var par = origin.parent();
+    var targetHash = origin[0].toString().substring(origin[0].href.indexOf('#'));
+    var target = $(targetHash);
+    target = target.length ? target : $('[name=' + targetHash.slice(1) +']');
+    if (target.length) {
+      var topPosition = target.offset().top - adjustment;
+      if(origin.attr('href').indexOf('home') > -1){
+        topPosition = 0;
+      }
+      $('html,body').animate({
+         scrollTop: topPosition
+        }, 600,function(){}
+      );
+      return false;
+    }
+  }
+}
+
+setAnimations = function(){
+  var win = $(window),
+      allMods = $('#portfolio li'),
+      logoDiv = $('section#logo');
+
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("is-visible");
+    }
+  });
+
+  win.scroll(function(event) {
+    if(window.scrollY > 50){
+      logoDiv.addClass('small');
+    } else {
+      logoDiv.removeClass('small');
+    }
+    allMods.each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("come-in");
+      }
+    });
+  });
+}
+
+setScrollNavPosition = function() {
+  var win = $(window),
+      sections = $('section[name]');
+
+  win.scroll(function(event) {
+    sections.each(function(i, el) {
+      var el = $(el),
+          activeNav = "",
+          itemWidth = 0,
+          left = 0;
+
+      if (el.visible(true)) {
+        activeNav = $('header a[href="#' + el.attr('name') + '"]');
+        itemWidth = activeNav[0].clientWidth;
+        left = activeNav.offset().left - $('header .row')[0].offsetLeft;
+        $('header a').removeClass('active');
+        navAnim.stopAnim = false;
+        navAnim.animate(navAnim.canvas, navAnim.context, left, itemWidth);
+        activeNav.addClass('active');
+      }
+    });
+  });
+}
+
+resizeNavAnimation = function() {
+  var activeNavItem = $('header a.active');
+  if(activeNavItem.length){
+    var itemWidth = $(activeNavItem)[0].clientWidth;
+    var left = $(activeNavItem).offset().left - $('header .row')[0].offsetLeft;
+    navAnim.stopAnim = false;
+    navAnim.animate(navAnim.canvas, navAnim.context, left, itemWidth);
+  }
+};
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-51701738-1', 'thejonlynch.com');
+ga('send', 'pageview');
